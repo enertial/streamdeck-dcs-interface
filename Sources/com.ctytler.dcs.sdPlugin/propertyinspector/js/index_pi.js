9eba23170598c3bcd1b5e94ae0afb375a929c3ed
Charlie Tytler <cjtytler@gmail.com>
2020-03-14 15:12:48 -0400

    Add default values, clear buttons, and other Javascript UI improvements

--- Sources/com.ctytler.dcs.sdPlugin/propertyinspector/js/index_pi.js
+++ Sources/com.ctytler.dcs.sdPlugin/propertyinspector/js/index_pi.js
@@ -69,8 +69,21 @@ $SD.on('connected', (jsn) => {
      */
 
     settings = Utils.getProp(jsn, 'actionInfo.payload.settings', false);
+
+    /* DEFAULT SETTINGS SPECIFIED HERE */
+    if (!settings.hasOwnProperty("press_value")) {
+        settings["press_value"] = 1;
+    }
+    if (!settings.hasOwnProperty("release_value")) {
+        settings["release_value"] = 0;
+    }
+    if (!settings.hasOwnProperty("dcs_id_comparison_value")) {
+        settings["dcs_id_comparison_value"] = 0;
+    }
+
     if (settings) {
         updateUI(settings);
+        console.log("Loaded settings: ", settings);
     }
 });
 
@@ -249,6 +262,40 @@ function sendSettingsToPlugin() {
 
         $SD.connection.send(JSON.stringify(json));
     }
+}
+
+
+/**
+ *  Clears setting for DCS Command Button ID, disabling the feature.
+ */
+function callbackClearDcsCommand() {
+    delete settings["button_id"];
+    $SD.api.setSettings($SD.uuid, settings);
+    button_id.value = "";
+    // Don't need to send to plugin as it always reads in latest settings on button push.
+    console.log("Clear DCS Command", settings);
+}
+
+/**
+ *  Clears setting for DCS ID Compare Monitor, disabling the feature.
+ */
+function callbackClearCompareMonitor() {
+    delete settings["dcs_id_compare_monitor"];
+    $SD.api.setSettings($SD.uuid, settings);
+    dcs_id_compare_monitor.value = "";
+    sendSettingsToPlugin();
+    console.log("Clear DCS ID Compare Monitor", settings);
+}
+
+/**
+ *  Clears setting for DCS ID String Monitor, disabling the feature.
+ */
+function callbackClearStringMonitor() {
+    delete settings["dcs_id_string_monitor"];
+    $SD.api.setSettings($SD.uuid, settings);
+    dcs_id_string_monitor.value = "";
+    sendSettingsToPlugin();
+    console.log("Clear DCS ID String Monitor", settings);
 }
 
 /** CREATE INTERACTIVE HTML-DOM
