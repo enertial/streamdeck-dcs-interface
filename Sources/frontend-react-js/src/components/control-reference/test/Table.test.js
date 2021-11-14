56709b12d8096cda04e6e1a759c4ac895f7d1c66
Charles Tytler <cjtytler@gmail.com>
2021-11-14 14:58:18 -0500

    Add Table.js unit tests

--- Sources/frontend-react-js/src/components/control-reference/test/Table.test.js
+++ Sources/frontend-react-js/src/components/control-reference/test/Table.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import Table from "../Table";
 
 const sampleControlSet = [
@@ -7,45 +7,49 @@ const sampleControlSet = [
     category: "Sample",
     description: "Description of control 1",
     control_type: "led",
+    additional_data: "Additional data 1"
   },
   {
     identifier: "control2",
     category: "Sample",
     description: "Description of control 2",
-    control_type: "led",
+    control_type: "selector",
+    additional_data: "Additional data 2"
   },
 ];
 
 test("renders loading message when isDataLoaded is false", () => {
-  render(<Table tableData={[]} isDataLoaded={false} onClick={() => {}} />);
+  render(<Table tableData={[]} isDataLoaded={false} onClick={() => { }} />);
   screen.getByText(/Loading module data/i);
-  expect(screen.queryByText(/Category/i)).toBeNull();
+  expect(screen.queryByRole("table")).toBeNull();
 });
 
-test("renders table with empty data", () => {
-  render(<Table tableData={[]} isDataLoaded={true} onClick={() => {}} />);
-  const table = screen.getByRole("table");
-  const firstColumnHeader = screen.getByText(/Category/i);
-  expect(firstColumnHeader).toBeInTheDocument();
-  expect(table).toBeInTheDocument();
+test("renders loading message when isDataLoaded is false even when data provided", () => {
+  render(<Table tableData={sampleControlSet} isDataLoaded={false} onClick={() => { }} />);
+  screen.getByText(/Loading module data/i);
+  expect(screen.queryByRole("table")).toBeNull();
 });
 
-test("renders table with sample control set", () => {
-  render(
-    <Table
-      tableData={sampleControlSet}
-      isDataLoaded={true}
-      onClick={() => {}}
-    />
-  );
+test("renders table with empty data when isDataLoaded is true", () => {
+  render(<Table tableData={[]} isDataLoaded={true} onClick={() => { }} />);
+  screen.getByRole("table");
+  screen.getByText(/Category/i); // Expect column header text to be present.
 });
 
 test("renders table with sample control set", () => {
-  render(
-    <Table
-      tableData={sampleControlSet}
-      isDataLoaded={true}
-      onClick={() => {}}
-    />
-  );
+  render(<Table tableData={sampleControlSet} isDataLoaded={true} onClick={() => { }} />);
+  screen.getByText(sampleControlSet[0].identifier);
+  const numRowsWithHeader = sampleControlSet.length + 1;
+  expect(screen.getAllByRole("row")).toHaveLength(numRowsWithHeader);
+});
+
+test("renders table with sample control set", () => {
+  let clickOutput;
+  const testClickFunction = (retData) => { clickOutput = retData };
+  render(<Table tableData={sampleControlSet} isDataLoaded={true} onClick={testClickFunction} />);
+  fireEvent.click(screen.getByText('control1'));
+
+  // Expect that data that does not show up in table (additional_data) is still returned onClick.
+  expect(screen.queryByText(/Additional data/i)).toBeNull();
+  expect(clickOutput.additional_data).toEqual("Additional data 1")
 });
