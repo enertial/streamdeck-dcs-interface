6bcae3a127d98fee8e46520b0dc456192c20ddd0
Charles Tytler <cjtytler@gmail.com>
2022-01-08 18:11:06 -0500

    Build tool: Fail build early if backend build fails

--- Tools/build_plugin.bat
+++ Tools/build_plugin.bat
@@ -10,6 +10,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tool
 
 :: Build C++ executable:
 devenv Sources\backend-cpp\Windows\com.ctytler.dcs.sdPlugin.sln /build "Release|x64"
+if %errorlevel% neq 0 echo "Canceling plugin build due to failed backend build" && pause && exit /b %errorlevel%
 
 :: Run unit tests, only continue if all tests pass
 Sources\backend-cpp\Windows\x64\Release\Test.exe
