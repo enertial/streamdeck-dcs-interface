3f81c9b06a084397f1056216a3159f6a5b671e97
Charles Tytler <cjtytler@gmail.com>
2022-02-06 13:13:27 -0500

    Add input/output descriptions on hover in SelectedControlRef modal

--- Sources/frontend-react-js/src/modals/SelectedControlRef.tsx
+++ Sources/frontend-react-js/src/modals/SelectedControlRef.tsx
@@ -1,7 +1,73 @@
 import { useDrag } from 'react-dnd'
-import { MouseEventHandler } from "react";
+import { MouseEventHandler, useState } from "react";
 import { ControlData, ControlInput, ControlOutputInteger, ControlOutputString } from "../api/DcsBios/ControlReferenceInterface"
 import classes from "./SelectedControlRef.module.css";
+
+interface InputProps {
+  identifier: string;
+  input: ControlInput;
+  onHover: (arg: string) => void;
+  onLeave: () => void;
+}
+const InputCard = ({ identifier, input, onHover, onLeave }: InputProps) => {
+  const [{ isDragging }, dragRef] = useDrag({
+    type: "dcsBiosInput",
+    item: { identifier },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+
+  function shareDescriptionOnHover() {
+    onHover(input.description);
+  }
+
+
+  return (
+    <div
+      className={classes.draggableCard}
+      ref={dragRef}
+      style={{ opacity: isDragging ? '0.5' : '1' }}
+      onMouseEnter={shareDescriptionOnHover}
+      onMouseLeave={onLeave}
+    >
+      Input.SetValue
+    </div>
+  )
+}
+
+interface OutputProps {
+  identifier: string;
+  output: ControlOutputInteger | ControlOutputString;
+  onHover: (arg: string) => void;
+  onLeave: () => void;
+}
+const OutputCard = ({ identifier, output, onHover, onLeave }: OutputProps) => {
+  const [{ isDragging }, dragRef] = useDrag({
+    type: output.type === "integer" ? "dcsBiosIntegerOutput" : "dcsBiosStringOutput",
+    item: { identifier },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+
+  function shareDescriptionOnHover() {
+    onHover(output.description);
+  }
+
+  return (
+    <div
+      className={classes.draggableCard}
+      ref={dragRef}
+      style={{ opacity: isDragging ? '0.5' : '1' }}
+      onMouseEnter={shareDescriptionOnHover}
+      onMouseLeave={onLeave}
+    >
+      {output.type === "integer" ? "Output.Integer" : "Output.String"}
+    </div>
+  )
+}
+
 
 interface Props {
   controlData: ControlData,
@@ -9,64 +75,47 @@ interface Props {
 }
 
 function SelectedControlRef({ controlData, onClick }: Props): JSX.Element {
+  const [hoverText, setHoverText] = useState("");
 
-  interface InputProps {
-    identifier: string;
-    input: ControlInput;
-  }
-  const InputCard = ({ identifier, input }: InputProps) => {
-    const [{ isDragging }, dragRef] = useDrag({
-      type: "dcsBiosInput",
-      item: { identifier },
-      collect: (monitor) => ({
-        isDragging: monitor.isDragging()
-      })
-    })
-
-    return (
-      <div className={classes.draggableCard} ref={dragRef}>
-        Input.SetValue
-      </div>
-    )
+  function onHover(description: string) {
+    setHoverText(description);
   }
 
-  interface OutputProps {
-    identifier: string;
-    output: ControlOutputInteger | ControlOutputString;
+  function onLeave() {
+    setHoverText("");
   }
-  const OutputCard = ({ identifier, output }: OutputProps) => {
-    const [{ isDragging }, dragRef] = useDrag({
-      type: output.type === "integer" ? "dcsBiosIntegerOutput" : "dcsBiosStringOutput",
-      item: { identifier },
-      collect: (monitor) => ({
-        isDragging: monitor.isDragging()
-      })
-    })
-
-    return (
-      <div className={classes.draggableCard} ref={dragRef}>
-        {output.type === "integer" ? "Output.Integer" : "Output.String"}
-      </div>
-    )
-  }
-
 
   return (
     <div className={classes.modal}>
       <h3>
         [{controlData.category}] {controlData.identifier}
       </h3>
-      <p>
+      <div className={classes.textArea}>
         <i>
           <b>Description: </b> {controlData.description}
         </i>
-      </p>
-      <p><b>Type:</b> {controlData.control_type}</p>
-      <div className={classes.draggableRow}>
-        {controlData.inputs.map((input) => (input.interface === "set_state" && <InputCard identifier={controlData.identifier} input={input} />))}
-        {controlData.outputs.map((output) => (<OutputCard identifier={controlData.identifier} output={output} />))}
+        <p>
+          <b>Type: </b> {controlData.control_type}
+        </p>
       </div>
-      <p><i>Drag input/output to use</i></p>
+      <div className={classes.draggableRow}>
+        {controlData.inputs.map((input) => (
+          input.interface === "set_state" &&
+          <InputCard
+            identifier={controlData.identifier}
+            input={input}
+            onHover={onHover}
+            onLeave={onLeave}
+          />))}
+        {controlData.outputs.map((output) => (
+          <OutputCard
+            identifier={controlData.identifier}
+            output={output}
+            onHover={onHover}
+            onLeave={onLeave}
+          />))}
+      </div>
+      <div className={classes.textArea}><i>{hoverText}</i></div>
       <button className="btn" onClick={onClick}>Close</button>
     </div>
   );
