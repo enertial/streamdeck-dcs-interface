b62123afe289c3d65d3b70d2ad491fa3dfce764c
Charles Tytler <cjtytler@gmail.com>
2022-02-07 23:21:20 -0500

    Add to PluginSetup modal

--- Sources/frontend-react-js/src/modals/PluginSetup.tsx
+++ Sources/frontend-react-js/src/modals/PluginSetup.tsx
@@ -27,17 +27,55 @@ function PluginSetup({ sdApi, hide }: Props): JSX.Element {
         <div>
             <div className={classes.popupwindow}>
                 <h2>Plugin Setup</h2>
-                Installation directory of DCS-BIOS: <br />
-                <input
-                    type="text"
-                    className={classes.pathInput}
-                    value={dcsBiosInstallPath}
-                    onChange={handleDcsBiosPathInputChange}
-                />
-                <button onClick={handleDcsBiosPathButtonPress}>Update</button>
-                <p>If you do not have DCS-BIOS installed you can download it and follow the "Quick Setup Guide" from
-                    <a href="https://github.com/DCSFlightpanels/dcs-bios">https://github.com/DCSFlightpanels/dcs-bios</a>
-                </p>
+                <div>
+                    Installation directory of DCS-BIOS: <br />
+                    <input
+                        type="text"
+                        className={classes.pathInput}
+                        value={dcsBiosInstallPath}
+                        onChange={handleDcsBiosPathInputChange}
+                    />
+                    <button className={classes.btn} onClick={handleDcsBiosPathButtonPress}>Update</button>
+                    <p>If you do not have DCS-BIOS installed you can download it and follow the "Quick Setup Guide" from
+                        <a href="https://github.com/DCSFlightpanels/dcs-bios"> https://github.com/DCSFlightpanels/dcs-bios</a>
+                    </p>
+                </div>
+                <div>
+                    <h3 className={classes.header}>Connection Settings</h3>
+                </div>
+                <div className={classes.grid}>
+                    <span className={classes.label}>IP Address: </span>
+                    <input
+                        type="text"
+                        className={classes.input}
+                        value="127.0.0.1"
+                        disabled={true}
+                    />
+                    <span className={classes.label}>Listener Port: </span>
+                    <input
+                        type="text"
+                        className={classes.input}
+                        value="5010"
+                        disabled={true}
+                    />
+                    <span className={classes.label}>Sender Port: </span>
+                    <input
+                        type="text"
+                        className={classes.input}
+                        value="7778"
+                        disabled={true}
+                    />
+                    <span className={classes.label}>Multicast Address: </span>
+                    <input
+                        type="text"
+                        className={classes.input}
+                        value="239.255.50.10"
+                        disabled={true}
+                    />
+                </div>
+                <p><i>Adjustable connection settings will be added later</i></p>
+                <br />
+                <button className="btn" onClick={hide}>Close</button>
             </div>
             <Backdrop onClick={hide} globalScope={true} />
         </div>
